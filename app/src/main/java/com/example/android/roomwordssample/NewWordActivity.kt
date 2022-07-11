e4cfa2426aebc1c658418166215c5b703bfd3f29
Johannes Beichter <34788506+jbeichter@users.noreply.github.com>
2022-07-11 19:33:49 +0200

    Migrate NewWordActivity to Compose

--- app/src/main/java/com/example/android/roomwordssample/NewWordActivity.kt
+++ app/src/main/java/com/example/android/roomwordssample/NewWordActivity.kt
@@ -16,36 +16,46 @@
 
 package com.example.android.roomwordssample
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 /**
  * Activity for entering a word.
  */
 
-class NewWordActivity : AppCompatActivity() {
+class NewWordActivity : ComponentActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_word)
-        val editWordView = findViewById<EditText>(R.id.edit_word)
-
-        val button = findViewById<Button>(R.id.button_save)
-        button.setOnClickListener {
-            val replyIntent = Intent()
-            if (TextUtils.isEmpty(editWordView.text)) {
-                setResult(Activity.RESULT_CANCELED, replyIntent)
-            } else {
-                val word = editWordView.text.toString()
-                replyIntent.putExtra(EXTRA_REPLY, word)
-                setResult(Activity.RESULT_OK, replyIntent)
+        setContent {
+            var word by rememberSaveable { mutableStateOf("") }
+            Column {
+                TextField(value = word, onValueChange = {
+                    word = it
+                })
+                Button(onClick = {
+                    val replyIntent = Intent()
+                    if (word.isEmpty()) {
+                        setResult(RESULT_CANCELED, replyIntent)
+                    } else {
+                        replyIntent.putExtra(EXTRA_REPLY, word)
+                        setResult(RESULT_OK, replyIntent)
+                    }
+                    finish()
+                }) {
+                    Text("Save")
+                }
             }
-            finish()
         }
     }
 
